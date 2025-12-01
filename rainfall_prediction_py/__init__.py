c1cc349827b64fb34e9772d571d3d1e6ce2e7eb3
agua-chile <150568085+agua-chile@users.noreply.github.com>
2025-11-30 17:28:41 -0700

    Refactor package initialization in __init__.py
    Removed docstring and updated author name format.

--- rainfall_prediction_py/__init__.py
+++ rainfall_prediction_py/__init__.py
@@ -1,20 +1,14 @@
-"""
-Package initialization for rainfall prediction project.
-"""
-
-__version__ = "1.0.0"
-__author__ = "Agua Chile"
-__description__ = "Australian Weather Rainfall Prediction Project"
+__version__ = '1.0.0'
+__author__ = 'agua-chile'
+__description__ = 'Australian Weather Rainfall Prediction Project'
 
 # Import main modules for easy access
-from . import config
 from . import data_processing
 from . import models
 from . import evaluation
 from . import visualization
 
 __all__ = [
-    'config',
     'data_processing', 
     'models',
     'evaluation',
